bc8de0b518b24d75a4a489476c83974244f51059
kareny51 <8mil.ton.511@gmail.com>
2015-04-26 20:24:31 +0900

    First attempt

---
+++ practice April 26th/practice April 26th/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice April 26th
+//
+//  Created by かれん 木元 on 26/04/2015.
+//  Copyright (c) 2015 karen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
